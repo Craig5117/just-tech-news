83fe9e3dbf2738ff63077bd935fc216530dfdb47
Craig5117 <craig.bennett5117@gmail.com>
2021-01-18 16:57:08 -0500

    login logout set up

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const morgan = require('morgan');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-// const morgan = require('morgan');
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 
@@ -13,7 +24,8 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
